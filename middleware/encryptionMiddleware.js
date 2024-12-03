e19c4b0ae57acc8da932b7308c918390b41a7d04
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2024-12-03 23:15:01 +0300

    change the encryption from crypto to JSencrypt

--- middleware/encryptionMiddleware.js
+++ middleware/encryptionMiddleware.js
@@ -1,8 +1,10 @@
-const crypto = require("crypto");
 const fs = require("fs");
+const JSEncrypt = require("node-jsencrypt");
 
+// Load the server's private key
 const serverPrivateKey = fs.readFileSync("./keys/private.pem", "utf8");
 
+// Middleware to decrypt the request body
 const decryptRequestBody = (req, res, next) => {
   const { public_key, encryptedData } = req.body;
 
@@ -13,15 +15,15 @@ const decryptRequestBody = (req, res, next) => {
   }
 
   try {
-    const buffer = Buffer.from(encryptedData, "base64");
-    const decryptedData = crypto.privateDecrypt(
-      {
-        key: serverPrivateKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      },
-      Buffer.from(encryptedData, "base64")
-    );
-    req.body = JSON.parse(decryptedData.toString("utf8"));
+    const decryptor = new JSEncrypt();
+    decryptor.setPrivateKey(serverPrivateKey);
+
+    const decryptedData = decryptor.decrypt(encryptedData);
+    if (!decryptedData) {
+      throw new Error("Decryption failed.");
+    }
+
+    req.body = JSON.parse(decryptedData);
     req.userPublicKey = public_key;
     next();
   } catch (err) {
@@ -30,26 +32,31 @@ const decryptRequestBody = (req, res, next) => {
   }
 };
 
+// Middleware to encrypt the response body
 const encryptResponseBody = (req, res, next) => {
   const originalSend = res.json;
+
   res.json = (data) => {
     try {
+      const encryptor = new JSEncrypt();
+      encryptor.setPublicKey(req.userPublicKey);
+
       const dataString = JSON.stringify(data);
-      const encryptedData = crypto.publicEncrypt(
-        {
-          key: req.userPublicKey,
-          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        },
-        Buffer.from(dataString)
-      );
+      const encryptedData = encryptor.encrypt(dataString);
+
+      if (!encryptedData) {
+        throw new Error("Encryption failed.");
+      }
+
       originalSend.call(res, {
-        encryptedData: encryptedData.toString("base64"),
+        encryptedData,
       });
     } catch (err) {
       console.error("Error encrypting response:", err);
       res.status(500).json({ message: "Error encrypting response" });
     }
   };
+
   next();
 };
 
