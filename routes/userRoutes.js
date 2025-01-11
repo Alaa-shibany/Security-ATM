1764576ca80dbedf659b905766ac52746cd33b5c
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-01-11 20:45:12 +0300

    change method for a route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const {
 
 const router = express.Router();
 
-router.post("/", getUserAccounts);
+router.get("/", getUserAccounts);
 router.post("/new", createAccountForUser);
 
 module.exports = router;
