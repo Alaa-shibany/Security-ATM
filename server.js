540ef4be008a4df45565d646d6ccebcb77f1caec
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-01-17 22:06:55 +0300

    move routes to app and isolate the server

--- server.js
+++ server.js
@@ -1,67 +1,10 @@
 const https = require("https");
 const fs = require("fs");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const app = require("./app");
 
 const { sequelize } = require("./models");
 
-const {
-  authMiddleware,
-  transactionalMiddleware,
-  symmetricDecrypt,
-  symmetricEncrypt,
-  configMiddleware,
-  signMiddleware,
-  verifyMiddleware,
-} = require("./middlewares");
-
-const {
-  authRoutes,
-  keyRoutes,
-  parkRoutes,
-  transactionRoutes,
-  userRoutes,
-} = require("./routes");
-
-const app = express();
 const PORT = 3000;
-
-app.use(
-  cors({
-    origin: "https://localhost:8000",
-  })
-);
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  console.log(req.path);
-  next();
-});
-
-app.use(transactionalMiddleware);
-app.use(configMiddleware);
-app.use("/key", keyRoutes);
-app.use("/auth", authRoutes);
-app.use(authMiddleware);
-app.use(symmetricDecrypt);
-app.use(verifyMiddleware);
-app.use("/accounts", userRoutes);
-app.use("/accounts", transactionRoutes);
-app.use("/park", parkRoutes);
-app.use("/*", (req, res, next) => {
-  if (req.final.status != 0) return next();
-  req.final.status = 404;
-  req.final.data = { error: "Not Found" };
-  next();
-});
-app.use(signMiddleware);
-app.use(symmetricEncrypt);
-
-//app.listen(PORT, async () => {
-//  await sequelize.sync({ force: false, alter: false, logging: false });
-//
-//  console.log(`Server is running on port ${PORT}`);
-//});
 
 const options = {
   key: fs.readFileSync("./certificates/server_private_key.pem"),
