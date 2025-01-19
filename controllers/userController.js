aaaf3369cb3bd01e1ee3f5c5884781b9eb3cd33d
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-01-19 20:14:19 +0300

    remove 404on account, because this scenario is possible the first time user enter hist account.

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,6 @@ const getUserAccounts = async (req, res, next) => {
       attributes: { exclude: ["createdAt", "updatedAt"] },
     });
 
-    if (!accounts || accounts.length === 0) {
-      req.final.status = 404;
-      req.final.data = { message: "No accounts found for the specified user" };
-      return next();
-    }
-
     req.final.status = 200;
     req.final.data = { message: "Accounts retrieved successfully", accounts };
     return next();
@@ -30,8 +24,11 @@ const createAccountForUser = async (req, res, next) => {
   try {
     if (req.final.status !== 0) return next();
     const userId = req.userId;
+    const { name, pin } = req.body;
     const newAccount = await Account.create({
       userId,
+      name,
+      pin,
       balance: 0,
     });
 
